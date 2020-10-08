644e72c1789c94f2f5b1a6b04dd0164dcd05a6bc
Lazzzer <lazazr@gmail.com>
2020-10-08 18:57:30 +0200

    feat(ex1) : Code clean manque commentaires + entête

--- Ex1.cpp
+++ Ex1.cpp
@@ -14,10 +14,12 @@ Compilateur : Mingw-w64 g++ 8.1.0
 
 /*
  * Note au groupe
- * std:oct si autorisé > http://www.cplusplus.com/reference/ios/oct/
- * logique si non-autorisé >
- * https://www.programiz.com/cpp-programming/examples/octal-decimal-convert
- * Reverse number: https://www.programiz.com/cpp-programming/examples/reverse-number
+ *
+ * Lien à citer dans les commentaires pour l'algo de changement de base:
+ * https://www.geeksforgeeks.org/program-decimal-octal-conversion/
+ *
+ * Reverse number:
+ * https://www.programiz.com/cpp-programming/examples/reverse-number
  */
 
 #include <cstdlib>
@@ -26,46 +28,58 @@ Compilateur : Mingw-w64 g++ 8.1.0
 using namespace std;
 
 int ex1() {
-	unsigned int entierDecimal;
 	const unsigned int BASE_OCTALE = 8;
 	const unsigned int BASE_DECIMALE = 10;
+	unsigned int entierDecimal;
 
-	cout << "Veuillez saisir un entier positif 'n': " << endl;
+	cout << "Veuillez saisir un entier positif \"n\" : " << endl;
 	cin >> entierDecimal;
 
+	// La valeur de l'entier saisi pouvant changer durant l'exécution du programme,
+	// on initialise une constante gardant sa valeur initiale.
 	const unsigned int ENTIER_INITIAL = entierDecimal;
 
-	unsigned int entierOctale = 0;
+	// Utilisation d'un unsigned long long car la valeur maximale d'un entier signé
+	// affiché sous sa forme octale ne peut pas être stockée avec 32 bits.
+	unsigned long long entierOctal;
 	bool estPalindrome;
 
-	estPalindrome = ENTIER_INITIAL < 10;
+	if (ENTIER_INITIAL >= BASE_OCTALE) {
+		unsigned int reste;
+		// Utilisation d'un unsigned long long car un débordement est possible lorsque
+		// de grosses valeurs sont insérées.
+		unsigned long long positionDecimale = 1;
 
-	if (ENTIER_INITIAL > BASE_OCTALE) {
-		unsigned int retenue;
-		unsigned int positionDecimale = 1;
+		entierOctal = 0;
 
 		while (entierDecimal != 0) {
-			retenue = entierDecimal % BASE_OCTALE;
+			reste = entierDecimal % BASE_OCTALE;
 			entierDecimal /= BASE_OCTALE;
-			entierOctale += retenue * positionDecimale;
+			entierOctal = entierOctal + reste * positionDecimale;
 			positionDecimale *= 10;
 		}
+		entierDecimal = ENTIER_INITIAL;
+	} else {
+		entierOctal = ENTIER_INITIAL;
 	}
-	entierDecimal = ENTIER_INITIAL;
+
+	estPalindrome = ENTIER_INITIAL < BASE_DECIMALE;
 
 	if (!estPalindrome) {
-		unsigned int retenue;
-		unsigned int entierInverse= 0;
+		unsigned int reste;
+		// Utilisation d'un unsigned long long car un débordement est possible lorsque
+		// de grosses valeurs sont insérées.
+		unsigned long long entierInverse = 0;
 
 		while (entierDecimal != 0) {
-			retenue = entierDecimal % BASE_DECIMALE;
-			entierInverse = entierInverse * BASE_DECIMALE + retenue;
+			reste = entierDecimal % BASE_DECIMALE;
 			entierDecimal /= BASE_DECIMALE;
+			entierInverse = entierInverse * BASE_DECIMALE + reste;
 		}
 		estPalindrome = entierInverse == ENTIER_INITIAL;
 	}
 
-	cout 	<< ENTIER_INITIAL << " en base 8 vaut " << entierOctale << endl;
+	cout << ENTIER_INITIAL << " en base 8 vaut " << entierOctal << endl;
 	if (estPalindrome) {
 		cout << ENTIER_INITIAL << " est un palindrome" << endl;
 	}
