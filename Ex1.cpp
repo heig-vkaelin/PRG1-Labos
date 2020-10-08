c01beed1b6ef3b363846b340c1183bb0ae8db8b6
Valentin Kaelin <kalanehd@gmail.com>
2020-10-08 21:30:55 +0200

    docs(ex1): add some comments

--- Ex1.cpp
+++ Ex1.cpp
@@ -1,26 +1,32 @@
 /*
 -----------------------------------------------------------------------------------
 Nom du fichier : Ex1.cpp
-Auteur(s) : Valentin Kaelin, Jonathan Friedli, Lazar Pavicevic
-Date creation : 08.10.2020
+Auteur(s)      : Valentin Kaelin, Jonathan Friedli, Lazar Pavicevic
+Date creation  : 09.10.2020
 
-Description : <à compléter>
+Description    : Programme demandant la saisie à l'utilisateur d'un entier "n".
+                 Le programme détermine en suite la valeur de "n" en octal et
+                 vérifie si "n" est un palindrome ou non. Pour finir, le programme
+                 affiche les résultats des opérations à l'écran.
 
-Remarque(s) : <à compléter>
+Remarque(s)    : On suppose la saisie utilisateur correcte.
 
-Compilateur : Mingw-w64 g++ 8.1.0
+                 La boucle while est autorisée mais les concepts de tableau ainsi
+                 que de chaîne de caractères ne le sont pas.
+
+                 Palindrome: nombre que l'on peut lire indifféremment de gauche à
+                 droite ou de droite à gauche (ex: 12021).
+
+                 Nous nous sommes inspirés de différents algorithmes trouvés sur
+                 le net pour la résolution de cet exercice:
+                 Changement de base:
+                 https://www.geeksforgeeks.org/program-decimal-octal-conversion/
+                 Inverser un nombre:
+                 https://www.programiz.com/cpp-programming/examples/reverse-number
+
+Compilateur    : Mingw-w64 g++ 8.1.0
 -----------------------------------------------------------------------------------
 */
-
-/*
- * Note au groupe
- *
- * Lien à citer dans les commentaires pour l'algo de changement de base:
- * https://www.geeksforgeeks.org/program-decimal-octal-conversion/
- *
- * Reverse number:
- * https://www.programiz.com/cpp-programming/examples/reverse-number
- */
 
 #include <cstdlib>
 #include <iostream>
@@ -36,7 +42,7 @@ int ex1() {
 	cin >> entierDecimal;
 
 	// La valeur de l'entier saisi pouvant changer durant l'exécution du programme,
-	// on initialise une constante gardant sa valeur initiale.
+	// on garde sa valeur initiale pour pouvoir l'utiliser à tout moment.
 	const unsigned int ENTIER_INITIAL = entierDecimal;
 
 	// Utilisation d'un unsigned long long car la valeur maximale d'un entier signé
@@ -52,12 +58,15 @@ int ex1() {
 
 		entierOctal = 0;
 
+		// Changement de base de l'entier initial vers la base octale
 		while (entierDecimal != 0) {
 			reste = entierDecimal % BASE_OCTALE;
 			entierDecimal /= BASE_OCTALE;
 			entierOctal = entierOctal + reste * positionDecimale;
 			positionDecimale *= 10;
 		}
+		// Remise de l'entier décimal à sa valeur initiale afin de pouvoir l'utiliser
+		// dans les futurs opérations pour déterminer s'il s'agit d'un palindrome
 		entierDecimal = ENTIER_INITIAL;
 	} else {
 		entierOctal = ENTIER_INITIAL;
@@ -68,9 +77,10 @@ int ex1() {
 	if (!estPalindrome) {
 		unsigned int reste;
 		// Utilisation d'un unsigned long long car un débordement est possible lorsque
-		// de grosses valeurs sont insérées.
+		// de grosses valeurs sont insérées (ex: 2'000'000'009 -> 9'000'000'002)
 		unsigned long long entierInverse = 0;
 
+		// Inversion de l'entier initial
 		while (entierDecimal != 0) {
 			reste = entierDecimal % BASE_DECIMALE;
 			entierDecimal /= BASE_DECIMALE;
@@ -82,7 +92,9 @@ int ex1() {
 	cout << ENTIER_INITIAL << " en base 8 vaut " << entierOctal << endl;
 	if (estPalindrome) {
 		cout << ENTIER_INITIAL << " est un palindrome" << endl;
+	} else {
+		cout << ENTIER_INITIAL << " n'est pas un palindrome" << endl;
 	}
+
 	return EXIT_SUCCESS;
 }
-
