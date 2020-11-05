62038008faec1a68e7446137a8bc837e8685be18
Valentin Kaelin <kalanehd@gmail.com>
2020-11-05 11:15:16 +0100

    feat: add some old functions to logic

--- Labo3/logique.cpp
+++ Labo3/logique.cpp
@@ -11,3 +11,25 @@ Remarque(s) : <à compléter>
 Compilateur : Mingw-w64 g++ 8.1.0
 -----------------------------------------------------------------------------------
 */
+
+bool estBissextile(unsigned annee) {
+	return annee % 400 == 0 || (annee % 4 == 0 && annee % 100 != 0);
+}
+
+unsigned nbJoursParMois(unsigned noMois, unsigned annee) {
+	enum class Mois {
+		JANVIER = 1, FEVRIER, MARS, AVRIL, MAI, JUIN, JUILLET, AOUT,
+		SEPTEMBRE, OCTOBRE, NOVEMBRE, DECEMBRE
+	};
+	switch ((Mois) noMois) {
+		case Mois::FEVRIER:
+			return estBissextile(annee) ? 29 : 28;
+		case Mois::AVRIL:
+		case Mois::JUIN:
+		case Mois::SEPTEMBRE:
+		case Mois::NOVEMBRE:
+			return 30;
+		default:
+			return 31;
+	}
+}
--- Labo3/logique.h
+++ Labo3/logique.h
@@ -15,4 +15,19 @@ Compilateur : Mingw-w64 g++ 8.1.0
 #ifndef LABO3_LOGIQUE_H
 #define LABO3_LOGIQUE_H
 
+/**
+ * Détermine si une année est bissextile ou pas
+ * @param annee
+ * @return vrai si l'année est bissextile, faux autrement
+ */
+bool estBissextile(unsigned annee);
+
+/**
+ * Détermine le nombre de jours d'un mois d'une année
+ * @param noMois
+ * @param annee
+ * @return nombre de mois
+ */
+unsigned nbJoursParMois(unsigned noMois, unsigned annee);
+
 #endif //LABO3_LOGIQUE_H
